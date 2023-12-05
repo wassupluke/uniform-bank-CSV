87ab56a09ced5970acf52c1015e752c8dd570237
Alyssa Wass <alyssawass@pop-os.localdomain>
2023-12-05 14:10:47 -0600

    abandonded, realized I already have this data in a spreadsheet

--- pdf2csv.py
+++ pdf2csv.py
@@ -3,15 +3,23 @@ import os
 import PyPDF2
 
 
-def print_paystub(path, paystub):
+def print_paystub(path: str, paystub: str) -> str:
     paystub = os.path.join(path, paystub)
     pdfFileObj = open(paystub, 'rb')
     pdfReader = PyPDF2.PdfReader(pdfFileObj)
-    total_pages = f'\n{paystub} has {len(pdfReader.pages)} pages.\n'
     content = [page.extract_text() for page in pdfReader.pages]
     content = ' '.join(content)
+    total_pages = f'\n{paystub} had {len(pdfReader.pages)} pages.\n'
     pdfFileObj.close()
-    return total_pages + content
+    return content + total_pages
+
+
+def tnaa_pdf2csv():
+    ...
+
+
+def mayo_pdf2csv():
+    ...
 
 
 # -------------------------------------------------------------------------- #
@@ -21,4 +29,7 @@ path = '/home/alyssawass/Documents/paystubs/'
 paystubs = os.listdir(path)
 
 for paystub in paystubs:
-    print(print_paystub(path, paystub))
+    content = print_paystub(path, paystub)
+    print(content)
+    content = content.split('\n')
+    content = [line.strip() for line in content if line != '']
