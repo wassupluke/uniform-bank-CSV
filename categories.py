7eea0c7c079e62a01430725423e00a572f478798
Luke Wass <wassupluke@gmail.com>
2023-12-10 21:59:45 -0700

    for working on at work if there is time

---
+++ categories.py
@@ -0,0 +1,23 @@
+categories = {
+        'Gas': [
+            'kwik trip',
+            ],
+        'Interest Credit': [
+            'interest credit',
+            ],
+        'Mortgage Payments': [
+            'flagstart loan',
+            ],
+        'Paychecks': [
+            'mayo foundation dir dep',
+            'travelnurseacros payroll',
+            ],
+        'Retirement Contributions': [
+            'edward jones investment',
+            ],
+        'Subscriptions': [
+            'adobe inc',
+            'patreon member',
+            ],
+
+        }
